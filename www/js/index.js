3988eaa3d786107d42e3c940c29580e4cee29bbf
Simon MacDonald <simon.macdonald@gmail.com>
2013-06-01 12:02:15 -0400

    getting PhoneGap to load

--- www/js/index.js
+++ www/js/index.js
@@ -33,17 +33,6 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicity call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
+        console.log("got deviceready");
     }
 };
